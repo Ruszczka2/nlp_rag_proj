f05dd9fddfbe7f4e75ae0661c856ce9063e368d5
Ruszczka2 <radoslawruczka@gmail.com>
2026-06-17 14:21:58 +0200

    chore: create folder structure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from nlp-rag-proj!")
+import numpy as np
 
+def main():
+    tablica = np.array([[0,1,2],[5,8,7]], dtype=np.int8)
+    print(tablica.shape)
 
 if __name__ == "__main__":
     main()
