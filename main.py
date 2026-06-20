0d2599c37b326d8c1bb036c91d5d19c23640a31a
Ruszczka2 <radoslawruczka@gmail.com>
2026-06-20 21:17:34 +0200

    feat: add parser commands for stem and lem

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from pathlib import Path
-import nlp_rag_proj as nlp
+from typing import Any
 import pandas as pd
 import argparse
 
+import spacy
+from functools import partial
+import Stemmer
 
-def main(model_path: Path = Path.cwd() / "models" / "tfidf_svc.joblib"):
+import nlp_rag_proj as nlp
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--submit", action="store_true", help="Wygeneruj plik submission_kaggle.csv")
-    args = parser.parse_args()
+
+def tfidf_svc_pipeline(args: Any | None = None, model_path: Path = Path.cwd() / "models" / "tfidf_svc.joblib"):
 
     df = nlp.io.load_bbc_csv(set_type="train")
 
     X_train, X_test, y_train, y_test = nlp.features.prepare_dataset(df, True)
 
+    if args.stem:
+        print("\nLoading stem model...\n")
+        stemmer = Stemmer.Stemmer('english')
+        text_normalization = partial(nlp.tokenization.stem, nlp_object=stemmer)
+    if args.lem:
+        print("\nLoading lemmatization model...\n")
+        nlp_obj = spacy.load("en_core_news_trf")
+        text_normalization = partial(nlp.tokenization.lemmatize, nlp_object=nlp_obj)
+        return 0
+    else:
+        text_normalization = nlp.clean.normalize_text
+
     if model_path.exists():
         model = nlp.predict.load_model()
     else:
         print(f"Nie znaleziono modelu w: {model_path}\n\nTrenuje model...\n")
-        model = nlp.train.train(X_train, y_train, model_path)
+        model = nlp.train.train(X=X_train, y=y_train, model_path=model_path, text_normalization=text_normalization)
 
-    y_pred = nlp.predict.predict_category(X_test, model)
+    y_pred = nlp.predict.predict_category(X_test=X_test, model=model)
 
     nlp.predict.evaluate_predictions(y_pred, y_test)
 
@@ -39,7 +53,15 @@ def main(model_path: Path = Path.cwd() / "models" / "tfidf_svc.joblib"):
             'Category': y_test_pred
         })
 
-        submission_df.to_csv(Path.cwd() / "data" / "outputs" / "submission_kaggle.csv", index=False)
+        submission_df.to_csv(Path.cwd() / "outputs" / "submission_kaggle.csv", index=False)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--submit", action="store_true", help="Wygeneruj plik submission_kaggle.csv")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-n", "--no-norm", action="store_true", help="Nie normalizuj tekstu")
+    group.add_argument("-st", "--stem", action="store_true", help="Stemming tekstu")
+    group.add_argument("-l", "--lem", action="store_true", help="Lematyzacja tekstu")
+    args = parser.parse_args()
+
+    tfidf_svc_pipeline(args)
