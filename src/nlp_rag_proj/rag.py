d21250dc17a7955d91008dc4c5f3a957682f759c
Ruszczka2 <radoslawruczka@gmail.com>
2026-06-25 14:10:44 +0200

    add calc_cosine_similarity

--- src/nlp_rag_proj/rag.py
+++ src/nlp_rag_proj/rag.py
@@ -1,8 +1,32 @@
 from nlp_rag_proj.clean import normalize_text
 from nlp_rag_proj.io import load_bbc_csv
 from pathlib import Path
+import pickle
 import pandas as pd
+import numpy as np
 import faiss
+
+def embed_query(query: str) -> np.ndarray:
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device='cuda')
+    return model.encode(query)
+
+def calc_cosine_similarity(query: str) -> np.ndarray[np.float32]:
+
+    with open(file=Path.cwd() / "data" / "emb" / "emb_dict.pickle", mode='rb') as f:
+        emb_dict = pickle.load(f, encoding='utf-8')
+
+    query_vect = embed_query(query)
+
+    embeddings_vect = np.array(emb_dict['embeddings'])
+
+    # Znormalizowane do 1 (L2) - nie muszę dzielić przez pierwiastki kwadratów v1 i v2 - wystarczy mnożenie macierzowe
+    similar_array = np.dot(embeddings_vect, query_vect)
+
+    #sorted_similar_array = similar_array[sorted_indices]
+    #id_sim = (sorted_idx, sorted_similar_array)
+    
+    return similar_array
 
 def chunk_text(text: str, chunk_size: int = 50, overlap: int = 10) -> list[str]:
 
@@ -22,7 +46,7 @@ def chunk_text(text: str, chunk_size: int = 50, overlap: int = 10) -> list[str]:
     chunks = [' '.join(text_list[i:i+chunk_size]) for i in range(0, len(text_list), step)]
     return chunks
 
-def build_index_from_bbc(df: pd.Series): # → embeddingi (np. sentence-transformers/all-MiniLM-L6-v2 lokalnie)
+def build_index_from_bbc(df: pd.Series) -> faiss.IndexFlatL2: # → embeddingi
 
     if not isinstance(df, pd.Series):
         raise TypeError("df must be pd.Series")
@@ -30,55 +54,33 @@ def build_index_from_bbc(df: pd.Series): # → embeddingi (np. sentence-transfor
     from sentence_transformers import SentenceTransformer
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device='cuda')
 
-    all_chunks = []
-    
-    df = load_bbc_csv(set_type="train")
-    text_list = df['text'].to_list()
-    print(len(text_list))
-    print(text_list[:10])
+    text_list = df.to_list()
     
     all_chunks = []
     for text in text_list:
         all_chunks.extend(chunk_text(text))
 
     embeddings = model.encode(all_chunks)
-    print(embeddings.shape)
-    print(embeddings[:10])
 
-    index = faiss.IndexFlatL2(embeddings.shape[1])
-    index.add(embeddings)
+    # Tu jest automatyczne wykorzystanie biblioteki faiss
+    # index = faiss.IndexFlatL2(embeddings.shape[1])
+    # index.add(embeddings)
 
-    print(index.ntotal)
+    output_file = Path.cwd() / "data" / "emb" / "emb_dict.pickle"
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(output_file, "wb") as f:
+        pickle.dump({'chunks': all_chunks,'embeddings': embeddings}, f)
     
+    return {'chunks': all_chunks,'embeddings': embeddings}
 
-    k = 4
-    xq = model.encode(["Someone demand a new car"])
-
-    D, I = index.search(xq, k)  # search
-    print(I)
-    print(type(I))
-    print(type(I[0]))
-    print(I)
-    print(I[0])
-
-    print("Długość text_list:", len(text_list))
-    print("Zawartość I[0]:", I[0].tolist())
-
-    # Bezpieczne pobieranie tekstów (odrzuca indeksy -1 oraz zbyt duże)
-    wyniki = [all_chunks[idx] for idx in I[0].tolist() if 0 <= idx < len(text_list)]
-    print(wyniki)
-
-
-    print([all_chunks[idx] for idx in I[0].tolist()])
-
-
-def build_index_from_docs(docs_dir: Path = Path.cwd() / "docs"): # → embeddingi (np. sentence-transformers/all-MiniLM-L6-v2 lokalnie)
+def build_index_from_docs(docs_dir: Path = Path.cwd() / "docs"): # → embeddingi
 
     if not docs_dir.is_dir():
         raise FileNotFoundError(f"Docs directory doesnt exist here: {docs_dir}")
     
-    # from sentence_transformers import SentenceTransformer
-    # model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    from sentence_transformers import SentenceTransformer
+    model = SentenceTransformer("sentence-transformers/distiluse-base-multilingual-cased-v1")
 
     all_chunks = []
     
@@ -94,27 +96,47 @@ def build_index_from_docs(docs_dir: Path = Path.cwd() / "docs"): # → embedding
     print(len(all_chunks))
     print(all_chunks[:10])
 
-    # for file_path in docs_dir.iterdir():
-    #     if file_path.is_file():
-    #         with open(file_path, 'r', encoding='utf-8') as file:
-    #             text = file.read()
-    #             text_chunked = chunk_text(text)
-    #             all_chunks.extend(text_chunked)
+    for file_path in docs_dir.iterdir():
+        if file_path.is_file():
+            with open(file_path, 'r', encoding='utf-8') as file:
+                text = file.read()
+                text_chunked = chunk_text(text)
+                all_chunks.extend(text_chunked)
 
-    # embeddings = model.encode(all_chunks)
-    # print(embeddings.shape)
-    # print(embeddings[:10])
+    embeddings = model.encode(all_chunks)
+    
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(embeddings)
 
-def retrieve(query, top_k): # → cosine similarity
-    pass
+    return index
+
+def retrieve(query: str, top_k: int = 4): # → cosine similarity
+    # xq = model.encode([query])
+    # _, I = index.search(xq, top_k)
+    # return(I)
+
+    similar_array = calc_cosine_similarity(query)
+
+    idx = np.arange(similar_array.shape[0])
+    # '-similar_array' - żeby malejąco
+    sorted_indices = np.argsort(-similar_array)
+    sorted_idx = idx[sorted_indices]
+
+    if top_k > sorted_idx.shape[0]:
+        raise ValueError(f"top_k ({top_k}) must be lower than {sorted_idx.shape[0]}")
+
+    idx_top_k = sorted_idx[:top_k]
+    print(idx_top_k)
 
 def answer(query): # → sklej kontekst + prompt do LLM (API lub Ollama lokalnie)
     pass
 
 if __name__ == "__main__":
-    df = load_bbc_csv(set_type="train")
-    text_sample = df['text'].iloc[0]
-    chunks = chunk_text(text_sample)
-    print(chunks)
-    print([len(x) for x in chunks])
-    build_index_from_bbc(df['text'])
+    with open(file=Path.cwd() / "data" / "emb" / "emb_dict.pickle", mode='rb') as f:
+        emb_dict = pickle.load(f, encoding='utf-8')
+    #retrieve('a person who is president of USA.', 5)
+
+    calc_cosine_similarity(('a person who is president of USA.'))
+
+    
+
