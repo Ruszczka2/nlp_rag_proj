8c72c4a854d43f769c02db0dda61e4e9ebb7a413
Ruszczka2 <radoslawruczka@gmail.com>
2026-06-25 20:08:27 +0200

    fix: make test_chunk pass

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -1,6 +1,4 @@
 from nlp_rag_proj.rag import chunk_text
 
-
-
-def test_chunk_text_():
-    assert chunk_text("a a a a a a a a a a a a a a a a a a a") == ['a a a a a a a a a a a a a a a a a a a', 'a a a']
+def test_chunk_text_division():
+    assert chunk_text("a "*60) == ["a "*49+"a", "a "*19+"a"]
