444547d8d6d39f5d0998fd4bcc8ea707c7a5b9ae
Ruszczka2 <radoslawruczka@gmail.com>
2026-06-25 10:21:29 +0200

    fix: get rid of for _ in range(1)

--- src/nlp_rag_proj/parrallel.py
+++ src/nlp_rag_proj/parrallel.py
@@ -2,14 +2,13 @@ from nlp_rag_proj.clean import normalize_text
 
         
 def process_single_file(full_path: str) -> None:
-    for _ in range(1):
-        with open(full_path, "r", encoding="utf-8") as plik:
-            text = plik.read()
-            text_normalized = normalize_text(text)
-            text_words = text_normalized.split()
-            len(text_words)
-            sorted(text_words)
-            sorted(text_words, key=len, reverse=True)
+    with open(full_path, "r", encoding="utf-8") as plik:
+        text = plik.read()
+        text_normalized = normalize_text(text)
+        text_words = text_normalized.split()
+        len(text_words)
+        sorted(text_words)
+        sorted(text_words, key=len, reverse=True)
     
 
 if __name__ == "__main__":
